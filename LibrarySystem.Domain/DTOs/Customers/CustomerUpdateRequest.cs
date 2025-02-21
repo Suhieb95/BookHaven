18457f953948522e9ea0056202550e1c347261e3
Sohaib <89126157+Suhieb95@users.noreply.github.com>
2025-02-22 03:20:13 +0400

    .

--- LibrarySystem.Domain/DTOs/Customers/CustomerUpdateRequest.cs
+++ LibrarySystem.Domain/DTOs/Customers/CustomerUpdateRequest.cs
@@ -1,4 +1,11 @@
+using LibrarySystem.Domain.DTOs.BaseModels;
 using Microsoft.AspNetCore.Http;
 
 namespace LibrarySystem.Domain.DTOs.Customers;
-public record class CustomerUpdateRequest(string EmailAddress, string Password, string UserName, string? ImageUrl = null, IFormFile? Image = null);
+public class CustomerUpdateRequest(string emailAddress, string password, string userName,
+string? imageUrl = null, IFormFile? image = null) : LoginRequestBase(emailAddress, password)
+{
+    public string UserName { get; init; } = userName;
+    public string? ImageUrl { get; init; } = imageUrl;
+    public IFormFile? Image { get; init; } = image;
+}
