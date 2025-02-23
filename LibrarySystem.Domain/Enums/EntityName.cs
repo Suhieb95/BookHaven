b97f8f85e50ac661ea430354c06d9f9dc4fe2809
Sohaib <89126157+Suhieb95@users.noreply.github.com>
2025-02-23 12:38:37 +0400

    Add Entity Name

--- LibrarySystem.Domain/Enums/EntityName.cs
+++ LibrarySystem.Domain/Enums/EntityName.cs
@@ -5,17 +5,15 @@ public enum EntityName : byte
     Authors = 1,
     Books = 2,
     BooksSales = 3,
-    BooksStock = 4,
-    Fines = 5,
     Genres = 6,
     Settings = 7,
     Users = 8,
-    Reservations = 9,
-    Borrowings = 10,
     BooksSalesDetails = 11,
     Customers = 12,
     UserRestPasswordApprovals = 13,
     Payments = 14,
     BookComments = 15,
-    BookImages = 16
+    BookImages = 16,
+    Shippings = 17,
+    Transactions = 18
 }
