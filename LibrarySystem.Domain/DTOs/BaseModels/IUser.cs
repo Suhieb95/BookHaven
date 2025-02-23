7d66e67478c84fbd0f6f5ffc05ca26b6a45e231c
Sohaib <89126157+Suhieb95@users.noreply.github.com>
2025-02-23 13:05:52 +0400

    .

--- LibrarySystem.Domain/DTOs/BaseModels/IUser.cs
+++ LibrarySystem.Domain/DTOs/BaseModels/IUser.cs
@@ -2,4 +2,5 @@ namespace LibrarySystem.Domain.DTOs.BaseModels;
 public interface IUser
 {
     IReadOnlyList<string> Permissions { get; }
+    string[] Roles { get; }
 }
--- LibrarySystem.Domain/DTOs/Users/InternalUserLoginResponse.cs
+++ LibrarySystem.Domain/DTOs/Users/InternalUserLoginResponse.cs
@@ -2,9 +2,11 @@ using LibrarySystem.Domain.BaseModels.User;
 using LibrarySystem.Domain.DTOs.BaseModels;
 
 namespace LibrarySystem.Domain.DTOs.Users;
-public class InternalUserLoginResponse(string emailAddress, string userName, string token, string imageurl, IReadOnlyList<string> permissions, Guid id)
+public class InternalUserLoginResponse(string emailAddress, string userName, string token, string imageurl, IReadOnlyList<string> permissions, Guid id, string[] roles)
   : AuthenticatedUserBase(emailAddress, userName, token, id), IUser
 {
   public IReadOnlyList<string> Permissions { get; } = permissions;
-  public string? ImageUrl { get; init; } = imageurl;
+  public string? ImageUrl { get; } = imageurl;
+  public string[] Roles { get; } = roles;
+
 };
